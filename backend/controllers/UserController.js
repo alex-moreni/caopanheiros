e938631bbc3103faab7de4afe3ea43fb41095fb8
AlexMoreni <alex.moreni47@gmail.com>
2024-06-19 14:40:32 -0300

    feat(backend): user token verification helper

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -139,4 +139,8 @@ module.exports = class UserController {
     res.status(200).json({ user })
 
   }
+
+  static async editUser (req, res) {
+    res.status(200).json({message: "Edit user"})
+  }
 }
