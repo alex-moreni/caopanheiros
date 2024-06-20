c5464a2cdc374715217313e7b89b27d95e04793a
AlexMoreni <alex.moreni47@gmail.com>
2024-06-20 14:48:50 -0300

    feat(backend): function of removing PET from the system

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -118,4 +118,32 @@ module.exports = class PetController {
     return res.status(200).json({pet})
 
   }
+
+  static async removePetById(req, res) {
+    const { id } = req.params
+
+    if(!ObjectID.isValid(id)) {
+      res.status(422).json({message: "Invalid ID"})
+      return
+    }
+
+    const pet = await Pet.findById(id)
+
+    if(!pet) {
+      res.status(404).json({message: "Pet not found"})
+      return
+    }
+
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    if(pet.user._id.toString() !== user.id.toString()) {
+      res.status(422).json({message: "Unauthorized"})
+      return
+    }
+
+    await Pet.findByIdAndRemove(id)
+
+    return res.status(200).json({message: "Pet deleted"})
+  }
 }
